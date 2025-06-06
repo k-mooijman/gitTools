8aa63c75f287c63ad3161e5002924327f9cac2af
Kasper Mooijman <kmooijman@bauwatch.com>
2025-06-06 16:58:36 +0200

    store

--- fe-src/src/pages/Blogs.js
+++ fe-src/src/pages/Blogs.js
@@ -1,5 +1,18 @@
+import TodoList from "../features/todos/TodoList";
+import Header from "../features/header/Header";
+import Footer from "../features/footer/Footer";
+
 const Blogs = () => {
-    return <h1>Blog Articles</h1>;
+    return <div>
+        <h1>Blog Articles 0</h1>
+        000
+        <Header />
+        111
+        <TodoList />
+        222
+        <Footer />
+        333
+    </div> ;
 };
 
 export default Blogs;
